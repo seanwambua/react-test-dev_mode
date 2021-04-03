89049a73fc7ad448861e6b8df4e188211509039f
Sean <seanwambua@gmail.com>
2021-04-03 03:45:10 +0300

    Setting up dynamic classes and styles

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -4,13 +4,26 @@ export default class Counter extends Component {
   state = {
     count: 0,
   };
+  styles = {
+    fontSize: 20,
+    fontWeight: "bold",
+  };
   render() {
     return (
       <div>
-        <span> {this.formatCount()}</span>
-        <button>Increment</button>
+        <span style={this.styles} className={this.getBadgeClasses()}>
+          {" "}
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
