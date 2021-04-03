b734aa2fa75889f4a4b30103504e2b10ffec8841
Sean <seanwambua@gmail.com>
2021-04-03 03:52:25 +0300

    Working with Lists

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -12,11 +12,6 @@ export default class Counter extends Component {
   render() {
     return (
       <div>
-        <span style={this.styles} className={this.getBadgeClasses()}>
-          {" "}
-          {this.formatCount()}
-        </span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
         <ul>
           {this.state.tags.map((tag) => (
             <li key={tag}>{tag}</li>
@@ -24,16 +19,5 @@ export default class Counter extends Component {
         </ul>
       </div>
     );
-  }
-
-  getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-    return classes;
-  }
-
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
   }
 }
