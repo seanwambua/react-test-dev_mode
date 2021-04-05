4e324bb97a2317724369e2ddd90f8340aefcdfb4
Sean <seanwambua@gmail.com>
2021-04-05 10:36:08 +0300

    Using React 'props'

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 export default class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
   };
 
   styles = {
@@ -18,7 +18,7 @@ export default class Counter extends Component {
           {this.formatCount()}
         </span>
         <button
-          onClick={() => this.handleIncrement({ id: this.state.count })}
+          onClick={() => this.handleIncrement({ id: this.state.value })}
           className="btn btn-primary btn-sm"
         >
           Increment
@@ -29,17 +29,17 @@ export default class Counter extends Component {
 
   handleIncrement = (product) => {
     console.log(product);
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value: value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
