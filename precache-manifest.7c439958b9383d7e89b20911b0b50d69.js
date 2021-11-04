af5292d37cd245974ba76b991c7a1c1ba7c3d809
Sean <seanwambua@gmail.com>
2021-11-04 14:38:35 +0300

    Updates

---
+++ precache-manifest.7c439958b9383d7e89b20911b0b50d69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2427f85ff04a183b76dfb15bae5f1fbe",
+    "url": "/react-test-dev_mode/index.html"
+  },
+  {
+    "revision": "826e02bf7181d684c771",
+    "url": "/react-test-dev_mode/static/css/2.5aa997f2.chunk.css"
+  },
+  {
+    "revision": "a0325d067bad673d8c68",
+    "url": "/react-test-dev_mode/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "826e02bf7181d684c771",
+    "url": "/react-test-dev_mode/static/js/2.5021a676.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-test-dev_mode/static/js/2.5021a676.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0325d067bad673d8c68",
+    "url": "/react-test-dev_mode/static/js/main.9d58a31f.chunk.js"
+  },
+  {
+    "revision": "9e6783d5bbdae7c35b73",
+    "url": "/react-test-dev_mode/static/js/runtime-main.60e31d96.js"
+  }
+]);
